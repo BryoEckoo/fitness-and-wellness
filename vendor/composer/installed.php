24717708cdb504c1d3c6e43d8777f4bea348fc42
Brian Rono <chikoe26@gmail.com>
2023-06-21 15:08:33 +0300

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'brian/restaurant',
+        'pretty_version' => 'v10.2.3',
+        'version' => '10.2.3.0',
+        'reference' => '85203d687ebba72b2805b89bba7d18dfae8f95c8',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'brian/restaurant' => array(
+            'pretty_version' => 'v10.2.3',
+            'version' => '10.2.3.0',
+            'reference' => '85203d687ebba72b2805b89bba7d18dfae8f95c8',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
